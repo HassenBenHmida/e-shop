18917f0aefa19968df959c0616cb87b837ac0aea
Hassen Ben Hmida <hassenbenhmida@hotmail.fr>
2020-03-16 18:05:05 +0100

    Working on the add to cart functionnality

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,15 +5,19 @@ import { ReactComponent as ShoppingCart } from "../../assets/cart.svg";
 import "./cart-icon.styles.scss";
 import { toggleCartDropdown } from "../../redux/cart/cart.actions";
 
-const CartIcon = ({ toggleCartDropdown }) => (
+const CartIcon = ({ toggleCartDropdown, cartitems }) => (
   <div className="cart-icon" onClick={toggleCartDropdown}>
     <ShoppingCart className="shopping-icon" />
     <span className="item-count">0</span>
   </div>
 );
 
+const mapStateToProps = state => ({
+  cartitems: state.cartitems
+})
+
 const mapDispatchToProps = dispatch => ({
   toggleCartDropdown: () => dispatch(toggleCartDropdown())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
